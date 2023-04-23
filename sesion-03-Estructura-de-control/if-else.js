b463ff0823c36b24e6d2c0b3bb9373207176fac7
oscar caceres <oscarcaceres.one@gmail.com>
2023-04-23 17:59:46 -0300

    creacion archivos if-else, if-else-ifelse, switch

---
+++ sesion-03-Estructura-de-control/if-else.js
@@ -0,0 +1,10 @@
+// Bifurcaciones if...else
+let saldo = 50;
+let efectivo = 20;
+
+if(efectivo < saldo){
+    console.log("Puede sacar dinero");
+} else {
+    console.log("Saldo insuficiente");
+}
+
