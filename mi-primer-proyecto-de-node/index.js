6904865357fb2ad4e9678451dfb2d793191b0025
oscar caceres <oscarcaceres.one@gmail.com>
2023-04-23 11:51:11 -0300

    Curso de js de Bootcam

---
+++ mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,12 @@
+
+// Este es el mensaje de bienvenida
+console.log("Estan es la puerta de entrada al pryecto");
+
+/*
+Esta es la parte intermedia del proyecyo
+ Esta parte realiamos lod chequeos necesarios
+ Por ultimo vamos a la ultima linea saludo
+*/
+
+// Este va a ser el mensaje que se envia para despedir
+console.log("Adios");
