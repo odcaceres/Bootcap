9641466a4d5074c08b1a34560d0fac52382fabc8
oscar caceres <oscarcaceres.one@gmail.com>
2023-04-23 15:30:36 -0300

    iniciacion - notacion

---
+++ iniciacion-notacion.js
@@ -0,0 +1,31 @@
+// Notacion
+
+//; --> Delimitar el final de la linea
+const b = 4;
+
+b + 4;
+
+
+// .  --> Se utiliza en los objetos para acceder a los atributos
+//movil.anchura
+
+// []  --> Listas, arreglos o arrays
+const ar = [1,2,3,4];
+console.log(ar[2]);
+
+// () --> Funciones
+function suma(a,b){
+    // Se escribe la funcion
+}
+
+// {} --> Llaves para objetos, funciones y estructura de control
+const movil = {
+    anchura: 5,
+    altura: 10
+}
+
+if(true){
+    // todo lo que hay entre llaves
+
+    const constantes2 = "hola";
+}
