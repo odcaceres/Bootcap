77ca62056bd852d072de8a07f56c755dd8a0b27f
oscar caceres <oscarcaceres.one@gmail.com>
2023-04-23 13:13:30 -0300

    Creacion de variables e iniciacion.

---
+++ iniciacion-tipos.js
@@ -0,0 +1,26 @@
+// Tipos primitivos
+
+// number
+4
+
+// string
+'Hola mundo';
+"Hola mundo";
+`Hola mundo`;
+
+// booleanos
+
+true;
+false;
+
+// nulos y undefined
+null;
+undefined;
+
+// null, undefined, false, 0  -> todas son false
+if(0){
+    console.log("Cumple")
+} else {
+    console.log("No cumple")
+}
+
---
+++ iniciacion-varialbes.js
@@ -0,0 +1,23 @@
+var variable;
+let variableLet;
+
+const constante = "Hola soy una constante";
+
+var a = 1;
+console.log(a);
+
+a = 4;
+console.log(a);
+
+console.log(constante);
+// constante = "Adiós";  // Esto nos da un error
+
+let b = 3;
+console.log(b);
+
+b = 5;
+console.log(b);
+
+//---------------------------------------------------------
+
+console.log(typeof b)
