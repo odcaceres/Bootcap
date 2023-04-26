ed1f3441e14293398f23d98046162cb793dbf7fc
oscar caceres <oscarcaceres.one@gmail.com>
2023-04-26 12:39:02 -0300

    Declaracion de variables numericas

---
+++ sesion05-numeros-y-presision/declaracion-de-variables-numericas.js
@@ -0,0 +1,18 @@
+// sesion - Numbers
+// Declaración de variables numéricas (enteros y con decimales)
+
+let a = 5;
+console.log(a);
+
+let b = 0.1;
+console.log(b);
+
+// Precisión
+let c = 0.2;
+console.log(c);
+
+console.log(b + c);
+
+// Pequeño truco para obtener valores "reales"
+console.log(Math.round((0.1 + 0.2) * 100) /100);
+console.log(Math.round((b + c) * 100) /100);
