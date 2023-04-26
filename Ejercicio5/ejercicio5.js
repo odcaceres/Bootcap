f6450eebcd977f8c0cafc114ecdcd20bc2071a84
oscar caceres <oscarcaceres.one@gmail.com>
2023-04-26 18:28:41 -0300

    Creacion de ejercicio5

---
+++ Ejercicio5/ejercicio5.js
@@ -0,0 +1,20 @@
+//Crea un archivo JS que contenga las siguientes líneas
+
+//- Una variable que contenga tu altura en centímetros (entero)
+let altura_cm = 173;
+
+//- Una variable que contenga tu altura en metros (número de coma flotante)
+let altura_m = 1.73;
+
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = 96.3;
+
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+let altura_redond = Math.ceil(altura_m);
+
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let peso_redond = Math.floor(peso);
+
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let sonIguales = (Number.MAX_VALUE + 1) === (Number.MAX_VALUE);
+
